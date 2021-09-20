d87d8f6812a3c19deb557a8d2d3b52c062eac6eb
Jon Keam <jpkeam@gmail.com>
2021-09-20 10:17:56 -0400

    fixed run command using auth for linux

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker run --rm --name simple-dotnet-builder -v "$(PWD):/app" -it mcr.microsoft.com/dotnet/sdk:5.0 /app/build.sh
 docker build -t simple-dotnet .
 # docker run --rm --name simple-dotnet -p "5000:5000" -p "5001:5001" -e ASPNETCORE_HTTPS_PORT=5001 simple-dotnet
-docker run --rm --name simple-dotnet -p 5000:5000 -p 5001:5001 -e ASPNETCORE_URLS="https://+;http://+" -e ASPNETCORE_HTTPS_PORT=5001 -e ASPNETCORE_Kestrel__Certificates__Default__Password="SECRETPASSWORD" -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx -v %USERPROFILE%\.aspnet\https:/https/ simple-dotnet
+docker run --rm --name simple-dotnet -p 5000:5000 -p 5001:5001 -e ASPNETCORE_URLS="https://+;http://+" -e ASPNETCORE_HTTPS_PORT=5001 -e ASPNETCORE_Kestrel__Certificates__Default__Password="SECRETPASSWORD" -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx -v ~/.aspnet/https:/https/ simple-dotnet
